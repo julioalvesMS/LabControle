98199dc232f21d91286e69e36eebcf38a551b535
Julio Alves <julioalves.mesquita@gmail.com>
2019-03-27 00:48:37 -0300

    Códigos Completos

--- plot_voltage_time_compare.m
+++ plot_voltage_time_compare.m
@@ -1,19 +1,25 @@
-function plot_voltage_time(y, t, name)
+function plot_voltage_time_compare(y, t, name)
 %PLOT_VOLTAGE_CURRENT Plot Voltage x Current
 %   Makes a Voltage X Current plot from the simulation output.
 %   The image will be saved automatically if the folder is argument 
 %   is given
     
-    name = strcat(name, ' - Saída de Tensão');
+    name = strcat(name, ' - Comparação dos Resultados');
 
     figure;
+    hold all;
     
-    plot(t, y);
+    for i=1:size(y, 2) 
+        plot(t, y(:,i));
+    end
     
     ylabel('y [V]');
     xlabel('t [s]');
     title(name);
     
+    legend('Saída Real', 'Saída Modelo');
+    
+    hold off;
     saveas(gcf, strcat('imagens/', name, '.png'));
 end
 
