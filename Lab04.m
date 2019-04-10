b74180bf5b8fa3620bc074e722af9beb3234ab8a
Julio Alves <julioalves.mesquita@gmail.com>
2019-04-10 01:14:14 -0300

    Pre relatorio feito

--- Lab04.m
+++ Lab04.m
@@ -19,15 +19,18 @@ G_den = den{1};
 
 C = 1e-6; % [F]
 
+f = 0.5;
+ta = 0:1e-6:10;
+ra = 0.5*square(2*pi*ta*f) + 0.5;
+
 %% Controlador PID
 
-Wosc = 86;
-Tosc = 2*pi/Wosc;
-Td = Tosc/8;
-Ti = Tosc/2;
-Kp = 2.1327;
+Td = 0.0107;
+Ti = 4.8778;
+Kp = 0.8790;
 tau = 0.01;
 Co = Kp*(1 + Td*s/(tau*s+1) + 1/(Ti*s));
+Co = minreal(Co);
 sys = pid(Co);
 
 Fo = Co*G/(1+Co*G);
@@ -45,14 +48,14 @@ sim('ziegler_nichols_2.slx', 10)
 %% Resistores
 
 R1_R2 = 1/(sys.ki * C);
-R1_R3 = sys.kp*R1_R2;
-R1R3 = sys.kd*R1_R2/C;
+R1_R3 = (sys.kp + sys.ki*tau)*R1_R2;
+R1R3 = (sys.kd + tau*sys.kp)*R1_R2/C;
 R1R2 = tau*R1_R2/C;
 
 %% Sistema ideal
 
-eq = [1 -R1_R2 R1R2]
-R1 = max(roots(eq));
+eq = [1 -R1_R2 R1R2];
+R1 = abs(min(roots(eq)));
 R2 = R1R2/R1;
 R3 = R1_R3 - R1;
 
@@ -61,17 +64,36 @@ Ci = minreal(Ci);
 
 Fi = Ci*G/(1+Ci*G);
 
-f = 0.5;
-ta = 0:1e-6:10;
-ra = 0.5*square(2*pi*ta*f) + 0.5;
 yi = lsim(Fi, ra, ta);
-plot_voltage_time_compare_model(yi, ta, y(:,2), y(:,1), 'Controlador Analógico')
+ui = lsim(Ci/(1+Ci*G), ra, ta);
+
+
+sistema = 'Controlador Equivalente';
+
+name = strcat(sistema, ' - Resposta - Comparação com o Objetivo');
+figure;
+hold all;
+plot(ta, yi);
+plot(y(:,1), y(:,2));
+ylabel('Tensão [V]');
+xlabel('Tempo [s]');
+title(name);
+legend('Controlador Analógico', 'Controlador Objetivo');
+hold off;
+saveas(gcf, strcat('imagens/', name, '.png'));
+
+plot_voltage_time_input(ui, ta, sistema)
 
 %% Criavel
 
-R1 = 15e3;
-R2 = 2.2e3;
-R3 = 22e3;
+% Valores Originais
+% R1 = 1.0018e+04;
+% R2 = 5.5392e+06;
+% R3 = 4.8778e+06;
+
+R1 = 1e+04;
+R2 = 8.2e+05;
+R3 = 8.2e+05;
 
 Cr = (R1*R3*C^2*s^2+ (R1+R3)*C*s+ 1)/(R1*R2*C^2*s^2+ (R1+R2)*C*s);
 Cr = minreal(Cr);
@@ -79,5 +101,20 @@ Cr = minreal(Cr);
 Fr = Cr*G/(1+Cr*G);
 
 yr = lsim(Fr, ra, ta);
+ur = lsim(Cr/(1+Cr*G), ra, ta);
 
-plot_voltage_time_compare_model(yr, ta, yi, ta, 'Controlador Analógico')
+sistema = 'Controlador Aplicável';
+
+name = strcat(sistema, ' - Resposta - Comparação com o Equivalente');
+figure;
+hold all;
+plot(ta, yr);
+plot(ta, yi);
+ylabel('Tensão [V]');
+xlabel('Tempo [s]');
+title(name);
+legend('Controlador Aplicável', 'Controlador Equivalente');
+hold off;
+saveas(gcf, strcat('imagens/', name, '.png'));
+
+plot_voltage_time_input(ur, ta, sistema)
